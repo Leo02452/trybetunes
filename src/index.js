42b3565dded45cc813bb85da6f259e5f80441f04
Leo Araujo <leonardo_02452@hotmail.com>
2022-04-02 16:31:29 -0300

    remove basename from index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/trybetunes">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
